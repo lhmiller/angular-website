f1573aac80ff52404bd63b1a3cab520906b9bbae
Tim Stoddard <tim.stoddard2@gmail.com>
2020-12-13 12:20:15 -0800

    improve home page

--- src/app/lucas/lucas.component.ts
+++ src/app/lucas/lucas.component.ts
@@ -1,22 +1,20 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
+const LOAD_TIME_MS = 800;
+const START_OFFSET_MS = 200;
+
 @Component({
   selector: 'app-lucas',
   templateUrl: './lucas.component.html',
   styleUrls: ['./lucas.component.scss']
 })
 export class LucasComponent implements OnInit, OnDestroy {
-  letters = 'Lucas Miller'.split('');
+  letters = 'Lucas Miller';
   count = 0;
   private timeouts: number[] = [];
 
   ngOnInit() {
-    for (let i = 0; i < this.letters.length; i++) {
-      const timeout = setTimeout(() => {
-        this.count++;
-      }, i * 200) as unknown as number;
-      this.timeouts.push(timeout);
-    }
+    this.animateLetters();
   }
 
   ngOnDestroy() {
@@ -25,8 +23,19 @@ export class LucasComponent implements OnInit, OnDestroy {
     }
   }
 
-  mapLettersToHTML = () => this.letters.map((letter: string) => letter === ' ' ? '&nbsp;' : letter);
+  animateLetters = () => {
+    this.count = 0;
+    const timeoutMs = LOAD_TIME_MS / this.letters.length;
+    for (let i = 0; i < this.letters.length; i++) {
+      const timeout = setTimeout(() => {
+        this.count++;
+      }, i * timeoutMs + START_OFFSET_MS) as unknown as number;
+      this.timeouts.push(timeout);
+    }
+  }
+
+  mapLettersToHTML = () => this.letters.split('').map((letter: string) => letter === ' ' ? '&nbsp;' : letter);
 
   isTriggeredClass = (i: number) =>
-    i <= this.count ? 'lucas__triggered' : 'lucas__notTriggered'
+    i < this.count ? 'lucas__triggered' : 'lucas__notTriggered'
 }
