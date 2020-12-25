4dd68d263f84964e961b7e584001f5a2f2000cc3
Tim Stoddard <tim.stoddard2@gmail.com>
2020-12-24 17:22:20 -0800

    add canvas to main page

--- src/app/lucas/lucas.component.ts
+++ src/app/lucas/lucas.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { TitleService } from '../shared/title.service';
 
 const LOAD_TIME_MS = 800;
 const START_OFFSET_MS = 200;
-
-// TODO idea
-// use a canvas as the background, draw a starry night
-// only update on resize
-// possible update every few seconds (1 star leaves, 1 added...?)
 
 @Component({
   selector: 'app-lucas',
@@ -15,21 +10,27 @@ const START_OFFSET_MS = 200;
   styleUrls: ['./lucas.component.scss']
 })
 export class LucasComponent implements OnInit, OnDestroy {
+  @ViewChild('canvas', { static: true }) canvasElement: ElementRef<HTMLCanvasElement>;
   letters = 'Lucas Miller';
   count = 0;
+  private canvas: CanvasRenderingContext2D;
   private timeouts: number[] = [];
 
   constructor(private titleService: TitleService) {}
 
   ngOnInit() {
+    this.canvas = this.canvasElement.nativeElement.getContext('2d');
+    this.updateCanvas();
     this.animateLetters();
     this.titleService.setTitle('Lucas Miller');
+    window.addEventListener('resize', this.updateCanvas);
   }
 
   ngOnDestroy() {
     for (const timeout of this.timeouts) {
       clearTimeout(timeout);
     }
+    window.removeEventListener('resize', this.updateCanvas);
   }
 
   animateLetters = () => {
@@ -47,4 +48,29 @@ export class LucasComponent implements OnInit, OnDestroy {
 
   isTriggeredClass = (i: number) =>
     i < this.count ? 'lucas__triggered' : 'lucas__notTriggered'
+
+  private updateCanvas = () => {
+    const {
+      clientWidth: viewportWidth,
+      clientHeight: viewportHeight,
+    } = document.documentElement;
+    this.canvasElement.nativeElement.width = viewportWidth;
+    this.canvasElement.nativeElement.height = viewportHeight;
+
+    // gray background
+    this.canvas.fillStyle = 'rgb(200,200,200)';
+    this.canvas.fillRect(0, 0, viewportWidth, viewportHeight);
+
+    // "stars"
+    for (let i = 0; i < 20; i++) {
+      const minPadding = 10;
+      const x = Math.floor(Math.random() * (viewportWidth - 2 * minPadding) + minPadding);
+      const y = Math.floor(Math.random() * (viewportHeight - 2 * minPadding) + minPadding);
+      const radius = Math.floor(Math.random() * 2 + 5);
+      this.canvas.beginPath();
+      this.canvas.arc(x, y, radius, 0, 2 * Math.PI);
+      this.canvas.fillStyle = 'white';
+      this.canvas.fill();
+    }
+  }
 }
